0c50f56450417f3b2aa8a986c88b0fb30c89041e
daniilsumilkin <danil.shumilkin.96@gmail.com>
2022-11-12 13:55:21 +0300

    добавлии инпут. Правка стилей

--- src/views/authorization/AuthWrapper.vue
+++ src/views/authorization/AuthWrapper.vue
@@ -1,10 +1,25 @@
 <template>
   <div class="auth-wrapper">
-    <base-icon name="clock" :size="52" />
+    <header class="auth-wrapper__header">
+      <base-icon name="clock" :size="52" />
+      <div>
+        <button
+          v-for="lang in $i18n.availableLocales"
+          :key="lang"
+          @click="$setLocale(lang)"
+        >
+          {{ lang }}
+        </button>
+      </div>
+    </header>
 
     <div class="auth-wrapper__inner">
       <div class="auth-wrapper__content">
-        <form novalidate @submit.prevent="$emit('submit')">
+        <form
+          class="auth-wrapper__form"
+          novalidate
+          @submit.prevent="$emit('submit')"
+        >
           <header class="auth-wrapper__content-header">
             <h1 class="auth-wrapper__content-title">
               {{ title }}
@@ -52,9 +67,17 @@ export default defineComponent({
   min-height: 100vh;
   box-sizing: border-box;
 }
+.auth-wrapper__header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.auth-wrapper__form {
+  width: 100%;
+}
 .auth-wrapper__inner {
   width: 100%;
-  height: calc(100vh - 120px);
+  height: calc(100vh - 220px);
   display: flex;
   align-items: center;
   justify-content: center;
@@ -64,7 +87,7 @@ export default defineComponent({
   display: flex;
   align-items: center;
   padding: 40px;
-  margin: 0 auto 20px;
+  margin: 0 auto;
   border-radius: 8px;
   box-shadow: 0 9px 40px 3px rgb($teal, 0.1);
   background-color: #fff;
--- src/views/authorization/LoginView.vue
+++ src/views/authorization/LoginView.vue
@@ -1,26 +1,31 @@
 <template>
   <auth-wrapper class="login-view" :title="$t('Login')">
-    <button
-      v-for="lang in $i18n.availableLocales"
-      :key="lang"
-      @click="$setLocale(lang)"
-    >
-      {{ lang }}
-    </button>
+    <base-input
+      class="mt-3"
+      v-model="email"
+      :label="$t('Email')"
+      model-type="email"
+    />
+    <base-input
+      class="mt-3"
+      v-model="password"
+      :label="$t('Password')"
+      model-type="password"
+    />
   </auth-wrapper>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import AuthWrapper from "@/views/authorization/AuthWrapper.vue";
+import BaseInput from "@/components/fields/BaseInput.vue";
 
 export default defineComponent({
   name: "LoginView",
-  components: { AuthWrapper },
+  components: { BaseInput, AuthWrapper },
+  data: () => ({
+    email: "",
+    password: "",
+  }),
 });
 </script>
-
-<style lang="scss">
-.login-view {
-}
-</style>
