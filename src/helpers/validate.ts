623d4bbdd00f6489ab223a57cc8c7f172d686ea7
daniilsumilkin <danil.shumilkin.96@gmail.com>
2023-01-01 19:23:40 +0300

    Сделали фасад для валидации

--- src/helpers/validate.ts
+++ src/helpers/validate.ts
@@ -1,16 +1,17 @@
 import { i18n } from "@/helpers/i18n";
 import * as validators from "@vuelidate/validators";
 import { ErrorObject } from "@vuelidate/core";
+import { keyBy } from "lodash-es";
 
 const { createI18nMessage } = validators;
 const withI18nMessage = createI18nMessage({ t: i18n.global.t.bind(i18n) });
 
-export type VErrorFields = {
+type VErrorFields = {
   readonly $property: string;
   readonly message: string;
 };
 
-export type VErrorObject = {
+type VErrorObject = {
   [key: string]: VErrorFields | ErrorObject;
 };
 
@@ -18,19 +19,41 @@ type Fields = {
   [key: string]: string;
 };
 
-export const required = withI18nMessage(validators.required, {
-  messagePath: (e) => e.$validator,
-});
+export const Validate = {
+  required: withI18nMessage(validators.required, {
+    messagePath: (e) => e.$validator,
+  }),
+  minLength: withI18nMessage(validators.minLength, {
+    withArguments: true,
+    messagePath: (e) => e.$validator,
+  }),
 
-export const minLength = withI18nMessage(validators.minLength, {
-  withArguments: true,
-  messagePath: (e) => e.$validator,
-});
+  sameAs: withI18nMessage(validators.sameAs, {
+    withArguments: true,
+    messagePath: (e) => e.$validator,
+  }),
 
-export const getEmptyErrors = (fields: Fields): VErrorFields[] =>
-  Object.values(fields).map((field): VErrorFields => {
+  getEmptyErrors: (fields: Fields): VErrorFields[] =>
+    Object.values(fields).map((field): VErrorFields => {
+      return {
+        $property: field,
+        message: "",
+      };
+    }),
+
+  getErrors: (errors: ErrorObject[]): ErrorObject[] => {
+    return errors.map((error) => {
+      return {
+        ...error,
+        message: String(error.$message),
+      };
+    });
+  },
+
+  getErrorByType(errors: ErrorObject[], fields: Fields): VErrorObject {
     return {
-      $property: field,
-      message: "",
+      ...keyBy(Validate.getEmptyErrors(fields), "$property"),
+      ...keyBy(errors, "$property"),
     };
-  });
+  },
+};
