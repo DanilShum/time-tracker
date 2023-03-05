b2e964567dc7416141c0c96c50915a34d0aca65b
daniilsumilkin <danil.shumilkin.96@gmail.com>
2023-03-05 14:42:26 +0300

    Обновили лоигку работы с сервисом

--- src/service/storage.ts
+++ src/service/storage.ts
@@ -1,29 +1,19 @@
 import { set, update } from "@/helpers/mutations";
-import { reactive, UnwrapRef } from "vue";
-
-interface IState<DTO> {
-  state: DTO;
-}
 
 export class Storage<T extends object> {
-  readonly observer: IState<UnwrapRef<T>>;
-  state: UnwrapRef<T>;
-  constructor(initialState: T) {
-    this.observer = reactive({ state: initialState });
-    this.state = this.observer.state;
+  state: T;
+  constructor(state: T) {
+    this.state = state;
   }
 
-  get(): UnwrapRef<T> {
-    return this.state;
-  }
-  set(state: UnwrapRef<T>): UnwrapRef<T> {
+  set(state: T): T {
     this.state = state;
     return this.state;
   }
-  update(state: Partial<UnwrapRef<T>>): void {
+  update(state: Partial<T>): void {
     update(this.state, state);
   }
-  setByKey(key: keyof UnwrapRef<T>, value: any): void {
+  setByKey(key: keyof T, value: any): void {
     set(this.state, key, value);
   }
 }
